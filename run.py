6e4e1747ef2e5b1f632f9b53ddc146903a0af207
luis-lop-nas <luislopnas@icloud.com>
2025-05-05 11:23:12 +0200

    cambios en la ventana

--- run.py
+++ run.py
@@ -5,9 +5,7 @@ import gestor.menu as menu
 import gestor.ui as ui
 
 def main():
-    """
-    Función principal que decide si lanzar modo terminal o modo gráfico.
-    """
+    
     if len(sys.argv) > 1 and sys.argv[1] == "-t":
         menu.iniciar()
     else:
